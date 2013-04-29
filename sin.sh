aa7fd6e45a07b561822fe43a6995f49a8b69c9fe
Sinjai Rakaii <sinjai@sinjai.tk>
2013-04-29 16:21:14 -0500

    Renaming

--- sin.sh
+++ sin.sh
@@ -3,12 +3,12 @@
 #Change these setting according to your setup.
 ################################################################################################################################################################
 default="normal"					##  "normal" = normal console mode, "silent" = silent background mode
-mcfdir="./"							##  Folder containing MCForge.exe
+sindir="./"							##  Folder containing SinCraft.exe
 monobin="/opt/mono-2.10/bin/mono"	##  Where is mono? Do not change if unsure. Typically "/usr/bin/mono"
 gameopt="--gc=sgen"					##  Mono garbage collector options, either "--gc=boehm" (older mono versions) or "--gc=sgen" (mono 2.8 or newer)
-gamename="RedCraft - MCForge"		##  Arbitrary name of server, will not affect actual server name
-gamepid="${mcfdir}/mcf.pid"			##  If you do not know what this is, do not worry about it, for "silent" mode only.
-gamelog="${mcfdir}/mcf.log"			##  This logs everything sent to console, if started in "silent" mode
+gamename="RedCraft - SinCraft"		##  Arbitrary name of server, will not affect actual server name
+gamepid="${mcfdir}/sin.pid"			##  If you do not know what this is, do not worry about it, for "silent" mode only.
+gamelog="${mcfdir}/sin.log"			##  This logs everything sent to console, if started in "silent" mode
 autorestart=true					##  set to false if you'd rather not auto-restart
 ################################################################################################################################################################
 #
@@ -27,8 +27,8 @@ else
 	exit 2
 fi
 
-if [ ! -f "${mcfdir}/MCForge.exe" ]; then
-	echo -e "cannot find ${mcfdir}/MCForge.exe!"
+if [ ! -f "${mcfdir}/SinCraft.exe" ]; then
+	echo -e "cannot find ${mcfdir}/SinCraft.exe!"
 	echo -e "If this is not correct edit the start script"
 	exit 2
 fi
@@ -37,15 +37,15 @@ case "$1" in
 	silent)
 		echo "Usage: $0 {stop|restart|status|check}"
 		echo -n "Starting $gamename server in silent background mode: "
-		if ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
+		if ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
 			echo -e "already active"
 			exit 0
 		else
 			if [ -f "${gamelog}" ]; then
 				cp ${gamelog} ${gamelog}.crash
 			fi
-			if ${monobin} ${gameopt} ${mcfdir}/MCForge.exe 1>> ${gamelog} 2>&1 & sleep 3 ; then
-				pid=`ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game { print $2 } ; END {exit status}'`
+			if ${monobin} ${gameopt} ${mcfdir}/SinCraft.exe 1>> ${gamelog} 2>&1 & sleep 3 ; then
+				pid=`ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game { print $2 } ; END {exit status}'`
 				echo ${pid} > ${gamepid}
 				if [ -f "${gamepid}" ] && ps h `cat "${gamepid}"` >/dev/null; then
 					echo -e "....Started!"
@@ -62,13 +62,13 @@ case "$1" in
 	stop)
 		echo "Usage: $0 {stop|restart|status|check}"
 		echo -n "Stopping $gamename server: "
-		if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
+		if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
 			echo -e "server not running or crashed."
 		else
-			pid=`ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game { print $2 } ; END {exit status}'`
+			pid=`ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game { print $2 } ; END {exit status}'`
 			echo ${pid} > ${gamepid}
 			kill -9 `cat ${gamepid}`
-			if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
+			if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
 				echo -e "stopped"
 				exit 0
 			else
@@ -79,7 +79,7 @@ case "$1" in
 	status)
 		echo "Usage: $0 {stop|restart|status|check}"
 		echo -n "`date +"%Y-%m-%d %H:%M:%S"` Checking $gamename server status: "
-		if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
+		if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
 			echo -e "server not running or crashed... Restarting"
 			$0
 		else
@@ -88,7 +88,7 @@ case "$1" in
 ;;
 	check)
 	        echo -n "Checking $gamename server status: "
-		if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
+		if ! ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
         	        echo -e "offline"
 	        else
 	                echo -e "online"
@@ -101,13 +101,13 @@ case "$1" in
 ;;
 	normal)
 		echo -n "Starting $gamename server with '${gameopt}' "
-		if ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/MCForge.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
+		if ps -ef |grep "${monobin} ${gameopt} ${mcfdir}/SinCraft.exe"|awk -F" " -v game=${monobin} 'BEGIN {status=1} ; $8 == game {status=0} ; END {exit status}' ; then
    			echo -e "already active"
 			exit 3
 		else
 			echo -e "--Hit CTRL+C multiple times to kill the script! Use '/save all' first, if you want to save"
 			echo -e
-			${monobin} ${gameopt} ${mcfdir}/MCForge.exe
+			${monobin} ${gameopt} ${mcfdir}/SinCraft.exe
 			if $autorestart ; then
 				$0
 			else
@@ -116,20 +116,20 @@ case "$1" in
 		fi
 ;;
 	*)
-		if [ -f "${mcfdir}/MCForge_.update" ]; then
-			if [ -f "${mcfdir}/MCForge.update" ]; then
+		if [ -f "${mcfdir}/SinCraft_.update" ]; then
+			if [ -f "${mcfdir}/SinCraft.update" ]; then
 				echo
 				echo Update Found!
 				echo -n Applying update:
-				rm ${mcfdir}/MCForge.exe
-				rm ${mcfdir}/MCForge_.dll
-				mv ${mcfdir}/MCForge.update ${mcfdir}/MCForge.exe
-				mv ${mcfdir}/MCForge_.update ${mcfdir}/MCForge_.dll
-				if [ -f "${mcfdir}/MCForge_.update" ]; then
-					if [ -f "${mcfdir}/MCForge.update" ]; then
+				rm ${mcfdir}/SinCraft.exe
+				rm ${mcfdir}/SinCraft_.dll
+				mv ${mcfdir}/SinCraft.update ${mcfdir}/SinCraft.exe
+				mv ${mcfdir}/SinCraft_.update ${mcfdir}/SinCraft_.dll
+				if [ -f "${mcfdir}/SinCraft_.update" ]; then
+					if [ -f "${mcfdir}/SinCraft.update" ]; then
 						echo -e FAILED!
-						if [ -f "${mcfdir}/MCForge_.dll" ]; then
-							if [ -f "${mcfdir}/MCForge.exe" ]; then
+						if [ -f "${mcfdir}/SinCraft_.dll" ]; then
+							if [ -f "${mcfdir}/SinCraft.exe" ]; then
 								$0 ${default}
 							fi
 						else
@@ -138,8 +138,8 @@ case "$1" in
 						fi
 					fi
 				else
-					if [ -f "${mcfdir}/MCForge_.dll" ]; then
-						if [ -f "${mcfdir}/MCForge.exe" ]; then
+					if [ -f "${mcfdir}/SinCraft_.dll" ]; then
+						if [ -f "${mcfdir}/SinCraft.exe" ]; then
 							echo -e SUCCESS!
 							$0 ${default}
 						fi
